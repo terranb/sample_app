233d087f3e16c809da15ce304b7028f96f2cbd70
Terran Birrell <terranrb@gmail.com>
2012-02-27 10:40:29 -0500

    Make a basic User model (including secure passwords)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
+	#logo helper
+  def logo
+    image_tag("logo.png", alt: "Sample App", class: "round")
+  end
+
 	# Returns the full title on a per-page basis.
-  def full_title(page_title)
+	def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
